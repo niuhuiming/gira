dbfee2aa1ac16844cdc00bcb3aa56029b8eead2a
nhm <3059350051@qq.com>
2022-11-28 12:21:18 +0800

    feat: 添加 useMount 和 useDebounce 两个hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
+/**
+ * 判断是否是有效值
+ */
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
-// 在一个函数里，改变传入的对象是不好的
+/**
+ * 过滤对象的空值
+ */
 export const cleanObject = (object) => {
+  // 在一个函数里，改变传入的对象是不好的
   const result = { ...object };
   Object.keys(result).forEach((key) => {
     const value = result[key];
@@ -10,4 +18,27 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+/**
+ * custom hook: 生命周期，挂载前
+ */
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+/**
+ * custom hook: 防抖，将传入的value变为防抖value
+ */
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
