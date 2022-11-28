bb3294fe7b3afd446ba792fcfd79ff770d7507fd
nhm <3059350051@qq.com>
2022-11-28 16:13:55 +0800

    chore: 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,17 +3,19 @@ import { useEffect, useState } from "react";
 /**
  * 判断是否是有效值
  */
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 /**
  * 过滤对象的空值
  */
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // 在一个函数里，改变传入的对象是不好的
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
@@ -23,16 +25,17 @@ export const cleanObject = (object) => {
 /**
  * custom hook: 生命周期，挂载前
  */
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
 /**
  * custom hook: 防抖，将传入的value变为防抖value
  */
-export const useDebounce = (value, delay) => {
+export const useDebounce = <T>(value: T, delay?: number): T => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
